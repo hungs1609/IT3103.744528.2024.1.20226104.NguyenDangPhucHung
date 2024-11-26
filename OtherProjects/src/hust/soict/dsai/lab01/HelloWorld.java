80b8cbe29d4514e355d1cf9972c095924d0918df
hungs1609 <hungndp1609@gmail.com>
2024-11-26 20:26:43 +0700

    update

--- OtherProjects/src/hust/soict/dsai/lab01/HelloWorld.java
+++ OtherProjects/src/hust/soict/dsai/lab01/HelloWorld.java
@@ -1,6 +1,10 @@
-package hust.soict.dsai.lab01;
+// Example 1: HelloWorld.java
+// Text-printing program
+
 public class HelloWorld {
-	public static void main(String[] arg) {
-		System.out.println("Hello, World");
-	}
+
+	public static void main(String args[]) {
+		System.out.println("Xin chao \ncac ban!");
+		System.out.println("Hello \t World!");
+	} // end of method main
 }
